33cc76de662565a8728c95a5d2d42add08db64ce
keyler3333 <kyturk42@gmail.com>
2026-04-20 20:20:04 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+}
+
 module.exports = nextConfig
