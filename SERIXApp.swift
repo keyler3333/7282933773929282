1815e9b94e5c2edf28ebd0937f0c9bc381ac25d4
keyler3333 <kyturk42@gmail.com>
2026-05-17 18:26:17 -0400

    Update SERIXApp.swift

--- SERIXApp.swift
+++ SERIXApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct SERIXApp: App {
     @StateObject private var orchestrator = TaskOrchestrator()
+    @StateObject private var settingsManager = SettingsManager()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(orchestrator)
+                .environmentObject(settingsManager)
         }
     }
 }
